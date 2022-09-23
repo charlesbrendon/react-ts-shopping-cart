0daafac64fdd5fb852a3e82c50a75720c9476f2f
Charles Brendon <charlesbrendonss@gmail.com>
2022-09-23 20:13:58 -0300

    import routes + create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home/Home";
+import { Store } from "./pages/Store/Store";
+import { About } from "./pages/About/About";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
+};
 
-export default App
+export default App;
